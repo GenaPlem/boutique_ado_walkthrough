0ac7651646afc21eaf88027314c87c53b6892028
GenaPlem <shainkill1337@gmail.com>
2023-12-07 12:55:24 +0000

    Fix typos

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -71,7 +71,7 @@ form.addEventListener('submit', function (ev) {
                         line2: $.trim(form.street_address2.value),
                         city: $.trim(form.town_or_city.value),
                         country: $.trim(form.country.value),
-//                        state: $.trim(form.state.value),
+                        state: $.trim(form.county.value),
                     }
                 }
             },
@@ -84,7 +84,7 @@ form.addEventListener('submit', function (ev) {
                     city: $.trim(form.town_or_city.value),
                     country: $.trim(form.country.value),
                     postal_code: $.trim(form.postcode.value),
-//                    state: $.trim(form.state.value),
+                    state: $.trim(form.county.value),
                 }
             }
         }).then(function (result) {
